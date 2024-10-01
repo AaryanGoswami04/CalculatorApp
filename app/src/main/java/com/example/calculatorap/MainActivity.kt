984f7398165f7ec5bc1d99fd330f273f617bb0c1
AaryanGoswami04 <aaryan2004.aa@gmail.com>
2024-10-01 10:34:41 +0530

    Preserving state of properties when layout is changed

--- app/src/main/java/com/example/calculatorap/MainActivity.kt
+++ app/src/main/java/com/example/calculatorap/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.calculatorap
 import android.media.VolumeShaper.Operation
 import android.os.Bundle
+import android.view.Display
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
@@ -12,6 +13,13 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import java.lang.Math.toRadians
 import kotlin.math.*
+//import kotlinx.android.synthetic.main.activity_main.*
+
+// To preserve operand and pendingOp when activity is destroyed:
+//These constants serve as keys for storing and retrieving specific pieces of data (operand1 and pendingOperation) in the Bundle object.
+private const val STATE_PENDING_OPERATION = "pendingOperation"
+private const val STATE_OPERAND1 = "operand1"
+private const val STATE_OPERAND1_STORED = "operand1_stored"
 
 class MainActivity : AppCompatActivity() {
     private var result: EditText? = null
@@ -19,7 +27,6 @@ class MainActivity : AppCompatActivity() {
     private val displayOperation by lazy {findViewById<TextView>(R.id.operation)} //another way to store widget reference
 
     private var operand1: Double? = null //operand1 can be null
-    private var operand2: Double = 0.0
     private var pendingOperation = "="
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -87,11 +94,15 @@ class MainActivity : AppCompatActivity() {
         fun getOperationListener(isBinaryOperation: Boolean): View.OnClickListener {
             return View.OnClickListener { v ->
                 val op = (v as Button).text.toString() //Clicked button's text is stored in op
-                val value = newNumber?.text.toString() //newNumber(input box)'s text is stored in value
-                if (value.isNotEmpty() && isBinaryOperation) {  //If no value is entered as i/p, skip, as binary operation can't be performed
-                    performBinaryOperation(value, op)
-                }else if(value.isNotEmpty() && !isBinaryOperation) {  //If no value is entered as i/p, skip, as binary operation can't be performed
-                    performUnaryOperation(value, op)
+                try {    //To prevent the app from crashing when . is passed as I/P and an operator button is clicked
+                    val value = newNumber?.text.toString().toDouble() //newNumber(input box)'s text is stored in value
+                    if (isBinaryOperation) {
+                        performBinaryOperation(value, op)
+                    } else {
+                        performUnaryOperation(value, op)
+                    }
+                }catch(e: NumberFormatException){ //When value = . (Exception)
+                    newNumber?.setText("")
                 }
                 pendingOperation = op
 
@@ -114,39 +125,67 @@ class MainActivity : AppCompatActivity() {
         buttonTan.setOnClickListener(getOperationListener(false))
 
     }
-    private fun performBinaryOperation(value: String, operation: String){
+    private fun performBinaryOperation(value: Double, operation: String){
         if(operand1 == null){ //If opr1 is null, no operand was previously entered, assign the entered value to operand1
-            operand1 = value.toDouble()
+            operand1 = value
         }
         else{
-            operand2 = value.toDouble()
-
             if(pendingOperation == "="){
                 pendingOperation = operation
             }
             when(pendingOperation){
-                "=" -> operand1 = operand2
-                "/" -> if(operand2 == 0.0) {  //division by Zero
+                "=" -> operand1 = value
+                "/" -> if(value == 0.0) {  //division by Zero
                             operand1 = Double.NaN
                         }else{
-                            operand1 = operand1!! / operand2 // !! ensures operand1 is only calculated if operand1 is not null
+                            operand1 = operand1!! / value// !! ensures operand1 is only calculated if operand1 is not null
                         }
-                "+" -> operand1 = operand1!! + operand2
-                "-" -> operand1 = operand1!! - operand2
-                "*" -> operand1 = operand1!! * operand2
+                "+" -> operand1 = operand1!! + value
+                "-" -> operand1 = operand1!! - value
+                "*" -> operand1 = operand1!! * value
 
             }
         }
 
     }
-    private fun performUnaryOperation(value: String, operation: String){
+    private fun performUnaryOperation(value:Double, operation: String){
 
         when(operation){
-            "sin" -> operand1 = sin(toRadians(value.toDouble()))
-            "cos" -> operand1 = cos(toRadians(value.toDouble()))
-            "tan" -> operand1 = tan(toRadians(value.toDouble()))
-            "log" -> if (value.toDouble() > 0) operand1 = log10(value.toDouble()) else operand1 = Double.NaN
+            "sin" -> operand1 = sin(toRadians(value))
+            "cos" -> operand1 = cos(toRadians(value))
+            "tan" -> operand1 = tan(toRadians(value))
+            "log" -> if (value > 0) operand1 = log10(value) else operand1 = Double.NaN
         }
     }
 
+    override fun onSaveInstanceState(outState: Bundle) { //To save the operand1, pending operation before activity is destroyed i.e when mode is changed from portrait to landscape
+        super.onSaveInstanceState(outState)
+        if(operand1 != null){ //If operand1 has a value (i.e., it's not null), it's saved into the Bundle using the key STATE_OPERAND1.
+                                // This ensures that the first operand is preserved across activity recreation.
+            outState.putDouble(STATE_OPERAND1, operand1!!)
+            outState.putBoolean(STATE_OPERAND1_STORED, true)
+        }
+        //Regardless of whether pendingOperation is null or not, it's saved into the Bundle using the key STATE_PENDING_OPERATION.
+        //his ensures that the current operation is preserved across activity recreation.
+        outState?.putString(STATE_PENDING_OPERATION, pendingOperation)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) { //Restores the state of the activity, ensuring that user data and UI elements maintain their state across activity recreation.
+        super.onRestoreInstanceState(savedInstanceState)
+
+       /* Checks a boolean flag (STATE_OPERAND1_STORED) to determine if
+           the first operand was saved before the activity was destroyed.
+           If the flag is true, it retrieves the stored double value associated with the key STATE_OPERAND1
+           If the flag is false, it sets operand1 to null, indicating that there is no first operand stored.*/
+        if(savedInstanceState.getBoolean(STATE_OPERAND1_STORED, false)){
+            operand1 = savedInstanceState.getDouble(STATE_OPERAND1)
+        }
+        else{
+            operand1 = null
+        }
+       // Retrieves the string value associated with the key STATE_PENDING_OPERATION, which represents the operation that has been selected by the user.
+        pendingOperation = savedInstanceState.getString(STATE_PENDING_OPERATION).toString()
+        displayOperation.text = pendingOperation
+
+    }
 }
